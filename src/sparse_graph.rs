db7c422234fb05f286b0b0bdf92a55594baeacca
Christian Fischer <christian@chfi.se>
2022-01-03 18:32:52 -0600

    parse GFA into a GraphBLAS adjacency matrix

--- src/sparse_graph.rs
+++ src/sparse_graph.rs
@@ -1,10 +1,89 @@
-use std::sync::Arc;
+use std::{io::BufReader, sync::Arc};
 
 use crate::sparse::*;
 
 use rayon::prelude::*;
+use rustc_hash::{FxHashMap, FxHashSet};
 
 use std::sync::Mutex;
+
+use lazy_static::lazy_static;
+
+use anyhow::Result;
+
+lazy_static! {
+    static ref GRAPHBLAS_CTX: Arc<Context> =
+        Context::init_ready(Mode::NonBlocking).unwrap();
+}
+
+pub fn gfa_to_adj_matrix(gfa_path: &str) -> Result<SparseMatrix<bool>> {
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    // use bstr::ByteSlice;
+
+    let file = File::open(gfa_path)?;
+    let mut reader = BufReader::new(file);
+
+    let mut buf = String::with_capacity(1024);
+
+    let mut segments: FxHashMap<usize, usize> = FxHashMap::default();
+    let mut links: FxHashSet<(usize, usize)> = FxHashSet::default();
+
+    loop {
+        buf.clear();
+        let readn = reader.read_line(&mut buf)?;
+
+        if readn == 0 {
+            break;
+        }
+
+        let line = &buf[..readn];
+
+        let mut fields = line.split("\t");
+
+        match fields.next() {
+            Some("S") => {
+                // let id = fields.next().map(|f| f.parse::<usize>()).flatten();
+                let id_str = fields.next().unwrap();
+                let id = id_str.parse::<usize>()?;
+
+                let seq = fields.next().unwrap();
+                segments.insert(id, seq.len());
+            }
+            Some("L") => {
+                let id_a_str = fields.next().unwrap();
+                let id_a = id_a_str.parse::<usize>()?;
+
+                let _orient = fields.next();
+
+                let id_b_str = fields.next().unwrap();
+                let id_b = id_b_str.parse::<usize>()?;
+
+                let left = id_a.min(id_b);
+                let right = id_a.max(id_b);
+
+                links.insert((left, right));
+            }
+            _ => (),
+        }
+    }
+
+    let n = segments.len();
+
+    let matrix_size = Size::new(n, n);
+    let mut adj_matrix: SparseMatrix<bool> =
+        SparseMatrix::new(&GRAPHBLAS_CTX, &matrix_size)?;
+
+    for link in links {
+        let row = link.0 - 1;
+        let column = link.1 - 1;
+        adj_matrix
+            .set_element(MatrixElement::from_triple(row, column, true))?;
+    }
+
+    Ok(adj_matrix)
+}
 
 fn parallel_calls_to_graphblas() {
     let context = Context::init_ready(Mode::NonBlocking).unwrap();
